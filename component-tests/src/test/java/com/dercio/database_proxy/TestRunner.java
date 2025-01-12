4bcd83277b2c7e048b366e6634c1480cb639ae40
Dercio Daio <daio.dercio2015@gmail.com>
2025-01-12 11:59:47 +0000

    Changed Test Runner to use junit5 instead junit4

--- component-tests/src/test/java/com/dercio/database_proxy/TestRunner.java
+++ component-tests/src/test/java/com/dercio/database_proxy/TestRunner.java
@@ -1,14 +1,14 @@
 package com.dercio.database_proxy;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"com.dercio.database_proxy"},
-        monochrome = true
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "com.dercio.database_proxy")
 public class TestRunner {
 }
